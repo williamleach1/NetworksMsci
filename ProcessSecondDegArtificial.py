84f05dcec52a771c1bbe3980ffc16564c550d6e7
williamleach1 <williamleach123@gmail.com>
2023-02-13 20:03:31 +0000

    Second and first degree artificial working
    Also good plots

--- ProcessSecondDegArtificial.py
+++ ProcessSecondDegArtificial.py
@@ -1,42 +1,337 @@
 from ProcessBase import *
-import warnings
-import scipy as sp
-from archive.Plotter import *
-warnings.filterwarnings("error")
-import graph_tool.all as gt
 
-start = time.time()
-def run_artificial(g):
-    """Perform all analysis on graph
-    Parameters  
-    ----------                  
-    g : graph_tool graph
-    Returns
-    -------     
-    df : dataframe
-        Dataframe containing results"""
+params =    {'font.size' : 16,
+            'axes.labelsize':16*2,
+            'axes.labelpad': 20,
+            'legend.fontsize': 14,
+            'xtick.labelsize': 14,
+            'ytick.labelsize': 14,
+            'axes.titlesize': 16,
+            'figure.titlesize': 16,
+            'figure.figsize': (12, 9),}
 
-    columns =   ["N", "E", "1/ln(z)", "1/ln(z) err", "Beta", "Beta err", "rchi",
-                 "pearson r", "pearson p-val", "spearmans r", "spearmans p-val",  
-                 "std_degree", "av_counts"]
+plt.rcParams.update(params)
 
-    k_2, c,popt,pcov, rchi, r, rp, rs, rsp, statistics_dict, mean_k_2= process(g, 2, to_print=False)
-    a = popt[0]
-    b = popt[1]
-    a_err = np.sqrt(pcov[0][0])
-    b_err = np.sqrt(pcov[1][1])
-    k_2s, inv_c_mean, errs, stds, counts   = unpack_stat_dict(statistics_dict)
-    av_counts = np.mean(counts)
+def package_2(g):
+    g = clean_graph(g)
+    k, c, popt,pcov, rchi, r, rp, rs, rsp, statistics_dict, mean_k = process(g,2,Real=False)
+    ks, inv_c_mean, errs, stds, counts   = unpack_stat_dict(statistics_dict)
+    unagg = [k, 1/c]
+    agg = [ks, inv_c_mean, errs]
+    agg_pred = [ks, Tim(ks, *popt)]
+    return unagg, agg, agg_pred
 
+def get_density(g):
     num_verticies = len(g.get_vertices())
     num_edges = len(g.get_edges())
+    # find densiy of network which is number of edges/number of possible edges
+    density = num_edges/(num_verticies*(num_verticies-1)/2)
+    return density
 
-    # find average and standard deviation of degree
-    avg_degree = mean_k_2
-    std_degree = np.std(k_2)
+def get_clustering(g):
+    (C, var_c) = gt.global_clustering(g)
+    return C
 
-    return final_df
 
 if __name__ == "__main__":
+
+    names = ['BA_K2','ER_K2']
+    MakeFolders(names, 'ArtificialUniNets')
+
+    # Plot one for BA - various mean k for one N
+    # Also do a collapse with multiple displayed on one plot
+
+    folder = "Output/ArtificialUniNets/BA_K2/"
+    figA, axsA = plt.subplots(1,1)
+
+    figs, axs = plt.subplots(1, 2, figsize=(15, 10))
+
+    N = 2000
+    av_degs = [10,20,40,80]
+
+    for i in range(len(av_degs)):
+        g = BA(N, av_degs[i])
+        unagg, agg, agg_pred = package_2(g)
+        #unagg_HO, agg_HO, agg_pred_HO = package_HO(g)
+
+        plt.figure()
+        plt.plot(unagg[0], unagg[1], 'o', label = r'$\langle k \rangle , N = $' + str(av_degs[i])+', '+str(N)) 
+        plt.xlabel(r"$k_{2}$")
+        plt.ylabel(r"$\frac{1}{c}$", rotation=0)
+        plt.xscale("log")
+        plt.legend()
+        plt.savefig(folder + 'BA_Unagg_'+str(N)+'_'+str(av_degs[i])+ '.svg', dpi = 900)
+        plt.close()
+
+        k, c = unagg
+        ks, inv_c_mean, errs = agg
+        ks, inv_c_pred = agg_pred
+        #ks_HO, inv_c_pred_HO = agg_pred_HO
+        axsA.errorbar(ks, inv_c_mean, yerr=errs, fmt='.' ,markersize = 5,capsize=2,color='black')
+        axsA.plot(ks, inv_c_mean,'o', label=r'$\langle k \rangle = $' + str(av_degs[i]))
+        axsA.plot(ks, inv_c_pred,'k--')
+        #axsA.plot(ks_HO, inv_c_pred_HO,'b--')
+
+        y = inv_c_mean/inv_c_pred
+        y_err = errs/inv_c_pred
+        axs[0].errorbar(ks, y, yerr=y_err, fmt='o' ,markersize = 2,capsize=2,color='black')
+        axs[0].plot(ks, y,'o', markersize = 5, label = r'$\langle k \rangle = $' + str(av_degs[i]))
+        
+
+        c_mean = 1/inv_c_mean
+        c_pred = 1/inv_c_pred
+        axs[1].plot(c_pred, c_mean, 'o', label = r'$\langle k \rangle = $' + str(av_degs[i]))
+
+    axsA.legend()
+    axsA.set_xlabel(r"$k_{2}$")
+    axsA.set_ylabel(r"$\frac{1}{c}$", rotation=0)
+    axsA.set_xscale("log")
+
+    min_k, max_k = axs[0].get_xlim()
+
+    fill_x = np.linspace(min_k*0.95, max_k*1.05, 100)
+
+    axs[0].fill_between(fill_x, 1-0.05, 1+0.05, color='grey', alpha=0.2, label=r'$\pm 5\%$')
+    axs[0].set_xlabel(r"$k_{2}$")
+    axs[0].set_ylabel(r"$\frac{\hat{c}}{c}$", rotation=0)
+    axs[0].legend()
+    axs[0].set_xscale("log")
+
+    min_y, max_y = axs[1].get_ylim()
+    min_x, max_x = axs[1].get_xlim()
+
+    c_pred = np.linspace(min_x*0.95, max_x*1.05, 100)
+
+    axs[1].plot(c_pred, c_pred, 'k--', label='Expected')
+    axs[1].fill_between(c_pred, c_pred*(1-0.05), c_pred*(1+0.05), color='grey', alpha=0.2, label=r'$\pm 5\%$')
+    axs[1].set_xlabel(r"$\hat{c}$", rotation=0)
+    axs[1].set_ylabel(r"$c$", rotation=0)
+    axs[1].legend()
+    figA.savefig(folder + 'Av_Deg_BA_N20000.svg', dpi = 900)
+    figs.savefig(folder+ 'Av_Deg_BA_collapse_N20000.svg', dpi= 900)
+
+    folder = "Output/ArtificialUniNets/BA_K2/"
+    figA, axsA = plt.subplots(1,1)
+
+    figs, axs = plt.subplots(1, 2, figsize=(15, 10))
+
+    Ns = [1000, 2000, 4000, 8000]
+    av_deg = 10
+
+    for i in range(len(Ns)):
+        g = BA(Ns[i], av_deg)
+        unagg, agg, agg_pred = package_2(g)
+        #unagg_HO, agg_HO, agg_pred_HO = package_HO(g)
+
+        plt.figure()
+        plt.plot(unagg[0], unagg[1], 'o', label = r'$\langle k \rangle , N = $' + str(Ns[i])+', '+str(av_deg)) 
+        plt.xlabel(r"$k_{2}$")
+        plt.ylabel(r"$\frac{1}{c}$", rotation=0)
+        plt.xscale("log")
+        plt.legend()
+        plt.savefig(folder + 'BA_Unagg_'+str(av_deg)+'_'+str(Ns[i])+ '.svg', dpi = 900)
+        plt.close()
+
+        k, c = unagg
+        ks, inv_c_mean, errs = agg
+        ks, inv_c_pred = agg_pred
+        #ks_HO, inv_c_pred_HO = agg_pred_HO
+        axsA.errorbar(ks, inv_c_mean, yerr=errs, fmt='.' ,markersize = 5,capsize=2,color='black')
+        axsA.plot(ks, inv_c_mean,'o', label=r'$N = $' + str(Ns[i]))
+        axsA.plot(ks, inv_c_pred,'k--')
+        #axsA.plot(ks_HO, inv_c_pred_HO,'b--')
+
+        y = inv_c_mean/inv_c_pred
+        y_err = errs/inv_c_pred
+        axs[0].errorbar(ks, y, yerr=y_err, fmt='o' ,markersize = 2,capsize=2,color='black')
+        axs[0].plot(ks, y,'o', markersize = 5, label = r'$N = $' + str(Ns[i]))
+        
+        c_mean = 1/inv_c_mean
+        c_pred = 1/inv_c_pred
+        axs[1].plot(c_pred, c_mean, 'o', label = r'$N = $' + str(Ns[i]))
+
+    axsA.legend()
+    axsA.set_xlabel(r"$k_{2}$")
+    axsA.set_ylabel(r"$\frac{1}{c}$", rotation=0)
+    axsA.set_xscale("log")
+
+    min_k, max_k = axs[0].get_xlim()
+
+    fill_x = np.linspace(min_k*0.95, max_k*1.05, 100)
+
+    axs[0].fill_between(fill_x, 1-0.05, 1+0.05, color='grey', alpha=0.2, label=r'$\pm 5\%$')
+    axs[0].set_xlabel(r"$k_{2}$")
+    axs[0].set_ylabel(r"$\frac{\hat{c}}{c}$", rotation=0)
+    axs[0].legend()
+    axs[0].set_xscale("log")
+
+    min_y, max_y = axs[1].get_ylim()
+    min_x, max_x = axs[1].get_xlim()
+
+    c_pred = np.linspace(min_x*0.95, max_x*1.05, 100)
+
+    axs[1].plot(c_pred, c_pred, 'k--', label='Expected')
+    axs[1].fill_between(c_pred, c_pred*(1-0.05), c_pred*(1+0.05), color='grey', alpha=0.2, label=r'$\pm 5\%$')
+    axs[1].set_xlabel(r"$\hat{c}$", rotation=0)
+    axs[1].set_ylabel(r"$c$", rotation=0)
+    axs[1].legend()
+    figA.savefig(folder + 'Ns_BA_k10.svg', dpi = 900)
+    figs.savefig(folder+ 'Ns_BA_collapse_k10.svg', dpi= 900)
+
+    # Plot one for ER - various mean k for one N
+    # Also do a collapse with multiple displayed on one plot
+
+    folder = "Output/ArtificialUniNets/ER_K2/"
+    figA, axsA = plt.subplots(1,1)
+
+    figs, axs = plt.subplots(1, 2, figsize=(15, 10))
+
+    N = 2000
+    av_degs = [10,20,40,80]
+
+    for i in range(len(av_degs)):
+        g = ER(N, av_degs[i])
+        unagg, agg, agg_pred = package_2(g)
+        #unagg_HO, agg_HO, agg_pred_HO = package_HO(g)
+
+        plt.figure()
+        plt.plot(unagg[0], unagg[1], 'o', label = r'$\langle k \rangle , N = $' + str(av_degs[i])+', '+str(N)) 
+        plt.xlabel(r"$k_{2}$")
+        plt.ylabel(r"$\frac{1}{c}$", rotation=0)
+        plt.xscale("log")
+        plt.legend()
+        plt.savefig(folder + 'ER_Unagg_'+str(N)+'_'+str(av_degs[i])+ '.svg', dpi = 900)
+        plt.close()
+
+        k, c = unagg
+        ks, inv_c_mean, errs = agg
+        ks, inv_c_pred = agg_pred
+        #ks_HO, inv_c_pred_HO = agg_pred_HO
+        axsA.errorbar(ks, inv_c_mean, yerr=errs, fmt='.' ,markersize = 5,capsize=2,color='black')
+        axsA.plot(ks, inv_c_mean,'o', label=r'$\langle k \rangle = $' + str(av_degs[i]))
+        axsA.plot(ks, inv_c_pred,'k--')
+        #axsA.plot(ks_HO, inv_c_pred_HO,'b--')
+
+        y = inv_c_mean/inv_c_pred
+        y_err = errs/inv_c_pred
+        axs[0].errorbar(ks, y, yerr=y_err, fmt='o' ,markersize = 2,capsize=2,color='black')
+        axs[0].plot(ks, y,'o', markersize = 5, label = r'$\langle k \rangle = $' + str(av_degs[i]))
+        
+        c_mean = 1/inv_c_mean
+        c_pred = 1/inv_c_pred
+        axs[1].plot(c_pred, c_mean, 'o', label = r'$\langle k \rangle = $' + str(av_degs[i]))
+
+    axsA.legend()
+    axsA.set_xlabel(r"$k_{2}$")
+    axsA.set_ylabel(r"$\frac{1}{c}$", rotation=0)
+    axsA.set_xscale("log")
+
+    min_k, max_k = axs[0].get_xlim()
+
+    fill_x = np.linspace(min_k*0.95, max_k*1.05, 100)
+
+    axs[0].fill_between(fill_x, 1-0.05, 1+0.05, color='grey', alpha=0.2, label=r'$\pm 5\%$')
+    axs[0].set_xlabel(r"$k_{2}$")
+    axs[0].set_ylabel(r"$\frac{\hat{c}}{c}$", rotation=0)
+    axs[0].legend()
+    axs[0].set_xscale("log")
+
+    min_y, max_y = axs[1].get_ylim()
+    min_x, max_x = axs[1].get_xlim()
+
+    c_pred = np.linspace(min_x*0.95, max_x*1.05, 100)
+
+    axs[1].plot(c_pred, c_pred, 'k--', label='Expected')
+    axs[1].fill_between(c_pred, c_pred*(1-0.05), c_pred*(1+0.05), color='grey', alpha=0.2, label=r'$\pm 5\%$')
+    axs[1].set_xlabel(r"$\hat{c}$", rotation=0)
+    axs[1].set_ylabel(r"$c$", rotation=0)
+    axs[1].legend()
+    figA.savefig(folder + 'Av_Deg_ER_N20000.svg', dpi = 900)
+    figs.savefig(folder+ 'Av_Deg_ER_collapse_N20000.svg', dpi= 900)
+
+    folder = "Output/ArtificialUniNets/ER_K2/"
+    figA, axsA = plt.subplots(1,1)
+
+    figs, axs = plt.subplots(1, 2, figsize=(15, 10))
+
+    Ns = [1000, 2000, 4000, 8000]
+    av_deg = 10
+
+    for i in range(len(Ns)):
+        g = ER(Ns[i], av_deg)
+        unagg, agg, agg_pred = package_2(g)
+        #unagg_HO, agg_HO, agg_pred_HO = package_HO(g)
+
+        plt.figure()
+        plt.plot(unagg[0], unagg[1], 'o', label = r'$\langle k \rangle , N = $' + str(Ns[i])+', '+str(av_deg)) 
+        plt.xlabel(r"$k_{2}$")
+        plt.ylabel(r"$\frac{1}{c}$", rotation=0)
+        plt.xscale("log")
+        plt.legend()
+        plt.savefig(folder + 'ER_Unagg_'+str(av_deg)+'_'+str(Ns[i])+ '.svg', dpi = 900)
+        plt.close()
+
+        k, c = unagg
+        ks, inv_c_mean, errs = agg
+        ks, inv_c_pred = agg_pred
+        #ks_HO, inv_c_pred_HO = agg_pred_HO
+        axsA.errorbar(ks, inv_c_mean, yerr=errs, fmt='.' ,markersize = 5,capsize=2,color='black')
+        axsA.plot(ks, inv_c_mean,'o', label=r'$N = $' + str(Ns[i]))
+        axsA.plot(ks, inv_c_pred,'k--')
+        #axsA.plot(ks_HO, inv_c_pred_HO,'b--')
+
+        y = inv_c_mean/inv_c_pred
+        y_err = errs/inv_c_pred
+        axs[0].errorbar(ks, y, yerr=y_err, fmt='o' ,markersize = 2,capsize=2,color='black')
+        axs[0].plot(ks, y,'o', markersize = 5, label = r'$N = $' + str(Ns[i]))
+        
+        c_mean = 1/inv_c_mean
+        c_pred = 1/inv_c_pred
+        axs[1].plot(c_pred, c_mean, 'o', label = r'$N = $' + str(Ns[i]))
+
+    axsA.legend()
+    axsA.set_xlabel(r"$k_{2}$")
+    axsA.set_ylabel(r"$\frac{1}{c}$", rotation=0)
+    axsA.set_xscale("log")
+
+    min_k, max_k = axs[0].get_xlim()
+
+    fill_x = np.linspace(min_k*0.95, max_k*1.05, 100)
+
+    axs[0].fill_between(fill_x, 1-0.05, 1+0.05, color='grey', alpha=0.2, label=r'$\pm 5\%$')
+    axs[0].set_xlabel(r"$k_{2}$")
+    axs[0].set_ylabel(r"$\frac{\hat{c}}{c}$", rotation=0)
+    axs[0].legend()
+    axs[0].set_xscale("log")
+
+    min_y, max_y = axs[1].get_ylim()
+    min_x, max_x = axs[1].get_xlim()
+
+    c_pred = np.linspace(min_x*0.95, max_x*1.05, 100)
+
+    axs[1].plot(c_pred, c_pred, 'k--', label='Expected')
+    axs[1].fill_between(c_pred, c_pred*(1-0.05), c_pred*(1+0.05), color='grey', alpha=0.2, label=r'$\pm 5\%$')
+    axs[1].set_xlabel(r"$\hat{c}$", rotation=0)
+    axs[1].set_ylabel(r"$c$", rotation=0)
+    axs[1].legend()
+    figA.savefig(folder + 'Ns_ER_k10.svg', dpi = 900)
+    figs.savefig(folder+ 'Ns_ER_collapse_k10.svg', dpi= 900)
+
+
+
+    # Plot density vs rchi for BA and ER
+
+
+
+    # Plot clustering vs rchi for BA and ER
+
+
+
+    # Plot assortativity vs rchi for BA and ER
+
+
+
+    # Contour plot of BA and ER reduced chi squared for various N and mean k
+
 
 
--- ProcessUniArtificial.py
+++ ProcessUniArtificial.py
@@ -1,20 +1,34 @@
-import os
-import seaborn as sns
-from matplotlib import cm
 from ProcessBase import *
-from datetime import datetime
-import csv
 
+params =    {'font.size' : 16,
+            'axes.labelsize':16*2,
+            'axes.labelpad': 20,
+            'legend.fontsize': 14,
+            'xtick.labelsize': 14,
+            'ytick.labelsize': 14,
+            'axes.titlesize': 16,
+            'figure.titlesize': 16,
+            'figure.figsize': (12, 9),}
+
+plt.rcParams.update(params)
 
 def package(g):
     g = clean_graph(g)
-    k, c, popt,pcov, rchi, r, rp, rs, rsp, statistics_dict, mean_k = process(g,1)
+    k, c, popt,pcov, rchi, r, rp, rs, rsp, statistics_dict, mean_k = process(g,1,Real=False)
     ks, inv_c_mean, errs, stds, counts   = unpack_stat_dict(statistics_dict)
-    rchi = red_chi_square(k,c,  )
-    unagg = [k, c]
+    unagg = [k, 1/c]
     agg = [ks, inv_c_mean, errs]
     agg_pred = [ks, Tim(ks, *popt)]
-    return unagg, agg, agg_pred, rchi
+    return unagg, agg, agg_pred
+
+def package_HO(g):
+    g = clean_graph(g)
+    k, c, popt,pcov, rchi, r, rp, rs, rsp, statistics_dict, mean_k = process(g,3, Real=False)
+    ks, inv_c_mean, errs, stds, counts   = unpack_stat_dict(statistics_dict)
+    unagg = [k, 1/c]
+    agg = [ks, inv_c_mean, errs]
+    agg_pred = [ks, HO_Tim(ks, *popt)]
+    return unagg, agg, agg_pred
 
 def get_density(g):
     num_verticies = len(g.get_vertices())
@@ -27,62 +41,287 @@ def get_clustering(g):
     (C, var_c) = gt.global_clustering(g)
     return C
 
+names = ['BA','ER']
+MakeFolders(names, 'ArtificialUniNets')
+
 # Plot one for BA - various mean k for one N
 # Also do a collapse with multiple displayed on one plot
 
+folder = "Output/ArtificialUniNets/BA/"
+figA, axsA = plt.subplots(1,1)
 
-plt.figure()
+figs, axs = plt.subplots(1, 2, figsize=(15, 10))
 
-for i in range
-plt.errorbar(ks, inv_c_mean, yerr=errs, fmt='.' ,markersize = 5,capsize=2,color='black')
-plt.plot(ks, inv_c_mean,'ro', label="Data")
-plt.plot(ks, Tim(ks, *popt),'b--', label="Fit to data")
+N = 2000
+av_degs = [10,20,40,80]
 
-plt.legend()
-plt.xlabel(r"$k$")
-plt.ylabel(r"$\frac{1}{c}$", rotation=0)
-plt.xscale("log")
-plt.title(names[i])
-plt.savefig(folder + 'inv_c_vs_k_agg.svg', dpi=900)
-plt.close()
+for i in range(len(av_degs)):
+    g = BA(N, av_degs[i])
+    unagg, agg, agg_pred = package(g)
+    #unagg_HO, agg_HO, agg_pred_HO = package_HO(g)
 
-# Now for collapse plot
-inv_c_pred = Tim(ks,a,b)
-y = inv_c_mean/inv_c_pred
-y_err = errs/inv_c_pred
-plt.figure()
-plt.title(names[i])
-# Shade +/- 0.05
-plt.fill_between(ks, 1-0.05, 1+0.05, color='grey', alpha=0.2, label=r'$\pm 5\%$')
-plt.errorbar(ks, y, yerr=y_err, fmt='.' ,markersize = 5,capsize=2,color='black')
-plt.plot(ks, y,'ro', label = 'Data')
-plt.xlabel("k")
-plt.ylabel(r"$\frac{\hat{c}}{c}$", rotation=0)
-plt.legend()
-plt.savefig(folder + 'inv_c_vs_k_collapse.svg', dpi=900)
-plt.close()
+    plt.figure()
+    plt.plot(unagg[0], unagg[1], 'o', label = r'$\langle k \rangle , N = $' + str(av_degs[i])+', '+str(N)) 
+    plt.xlabel(r"$k$")
+    plt.ylabel(r"$\frac{1}{c}$", rotation=0)
+    plt.xscale("log")
+    plt.legend()
+    plt.savefig(folder + 'BA_Unagg_'+str(N)+'_'+str(av_degs[i])+ '.svg', dpi = 900)
+    plt.close()
 
-plt.figure()
-plt.title(names[i])
-c_mean = 1/inv_c_mean
-c_pred = 1/inv_c_pred
-plt.plot(c_pred, c_mean, 'ro')
-plt.ylabel(r"$c$", rotation=0)
-plt.xlabel(r"$\hat{c}$", rotation=0)
-# Add line at 45 degrees and shade +/- 5%
-plt.plot(c_pred, c_pred, 'k--', label='Expected')
-plt.fill_between(1/inv_c_pred, 1/inv_c_pred*(1-0.05), 1/inv_c_pred*(1+0.05), color='grey', alpha=0.2, label=r'$\pm 5\%$')
+    k, c = unagg
+    ks, inv_c_mean, errs = agg
+    ks, inv_c_pred = agg_pred
+    #ks_HO, inv_c_pred_HO = agg_pred_HO
+    axsA.errorbar(ks, inv_c_mean, yerr=errs, fmt='.' ,markersize = 5,capsize=2,color='black')
+    axsA.plot(ks, inv_c_mean,'o', label=r'$\langle k \rangle = $' + str(av_degs[i]))
+    axsA.plot(ks, inv_c_pred,'k--')
+    #axsA.plot(ks_HO, inv_c_pred_HO,'b--')
 
-plt.savefig(folder + 'c_vs_c_hat.svg', dpi=900)
-plt.close()
+    y = inv_c_mean/inv_c_pred
+    y_err = errs/inv_c_pred
+    axs[0].errorbar(ks, y, yerr=y_err, fmt='o' ,markersize = 2,capsize=2,color='black')
+    axs[0].plot(ks, y,'o', markersize = 5, label = r'$\langle k \rangle = $' + str(av_degs[i]))
+    
 
+    c_mean = 1/inv_c_mean
+    c_pred = 1/inv_c_pred
+    axs[1].plot(c_pred, c_mean, 'o', label = r'$\langle k \rangle = $' + str(av_degs[i]))
 
+axsA.legend()
+axsA.set_xlabel(r"$k$")
+axsA.set_ylabel(r"$\frac{1}{c}$", rotation=0)
+axsA.set_xscale("log")
 
+min_k, max_k = axs[0].get_xlim()
 
+fill_x = np.linspace(min_k*0.95, max_k*1.05, 100)
 
+axs[0].fill_between(fill_x, 1-0.05, 1+0.05, color='grey', alpha=0.2, label=r'$\pm 5\%$')
+axs[0].set_xlabel(r"$k$")
+axs[0].set_ylabel(r"$\frac{\hat{c}}{c}$", rotation=0)
+axs[0].legend()
+axs[0].set_xscale("log")
+
+min_y, max_y = axs[1].get_ylim()
+min_x, max_x = axs[1].get_xlim()
+
+c_pred = np.linspace(min_x*0.95, max_x*1.05, 100)
+
+axs[1].plot(c_pred, c_pred, 'k--', label='Expected')
+axs[1].fill_between(c_pred, c_pred*(1-0.05), c_pred*(1+0.05), color='grey', alpha=0.2, label=r'$\pm 5\%$')
+axs[1].set_xlabel(r"$\hat{c}$", rotation=0)
+axs[1].set_ylabel(r"$c$", rotation=0)
+axs[1].legend()
+figA.savefig(folder + 'Av_Deg_BA_N20000.svg', dpi = 900)
+figs.savefig(folder+ 'Av_Deg_BA_collapse_N20000.svg', dpi= 900)
+
+folder = "Output/ArtificialUniNets/BA/"
+figA, axsA = plt.subplots(1,1)
+
+figs, axs = plt.subplots(1, 2, figsize=(15, 10))
+
+Ns = [1000, 2000, 4000, 8000]
+av_deg = 10
+
+for i in range(len(Ns)):
+    g = BA(Ns[i], av_deg)
+    unagg, agg, agg_pred = package(g)
+    #unagg_HO, agg_HO, agg_pred_HO = package_HO(g)
+
+    plt.figure()
+    plt.plot(unagg[0], unagg[1], 'o', label = r'$\langle k \rangle , N = $' + str(Ns[i])+', '+str(av_deg)) 
+    plt.xlabel(r"$k$")
+    plt.ylabel(r"$\frac{1}{c}$", rotation=0)
+    plt.xscale("log")
+    plt.legend()
+    plt.savefig(folder + 'BA_Unagg_'+str(av_deg)+'_'+str(Ns[i])+ '.svg', dpi = 900)
+    plt.close()
+
+    k, c = unagg
+    ks, inv_c_mean, errs = agg
+    ks, inv_c_pred = agg_pred
+    #ks_HO, inv_c_pred_HO = agg_pred_HO
+    axsA.errorbar(ks, inv_c_mean, yerr=errs, fmt='.' ,markersize = 5,capsize=2,color='black')
+    axsA.plot(ks, inv_c_mean,'o', label=r'$N = $' + str(Ns[i]))
+    axsA.plot(ks, inv_c_pred,'k--')
+    #axsA.plot(ks_HO, inv_c_pred_HO,'b--')
+
+    y = inv_c_mean/inv_c_pred
+    y_err = errs/inv_c_pred
+    axs[0].errorbar(ks, y, yerr=y_err, fmt='o' ,markersize = 2,capsize=2,color='black')
+    axs[0].plot(ks, y,'o', markersize = 5, label = r'$N = $' + str(Ns[i]))
+    
+    c_mean = 1/inv_c_mean
+    c_pred = 1/inv_c_pred
+    axs[1].plot(c_pred, c_mean, 'o', label = r'$N = $' + str(Ns[i]))
+
+axsA.legend()
+axsA.set_xlabel(r"$k$")
+axsA.set_ylabel(r"$\frac{1}{c}$", rotation=0)
+axsA.set_xscale("log")
+
+min_k, max_k = axs[0].get_xlim()
+
+fill_x = np.linspace(min_k*0.95, max_k*1.05, 100)
+
+axs[0].fill_between(fill_x, 1-0.05, 1+0.05, color='grey', alpha=0.2, label=r'$\pm 5\%$')
+axs[0].set_xlabel(r"$k$")
+axs[0].set_ylabel(r"$\frac{\hat{c}}{c}$", rotation=0)
+axs[0].legend()
+axs[0].set_xscale("log")
+
+min_y, max_y = axs[1].get_ylim()
+min_x, max_x = axs[1].get_xlim()
+
+c_pred = np.linspace(min_x*0.95, max_x*1.05, 100)
+
+axs[1].plot(c_pred, c_pred, 'k--', label='Expected')
+axs[1].fill_between(c_pred, c_pred*(1-0.05), c_pred*(1+0.05), color='grey', alpha=0.2, label=r'$\pm 5\%$')
+axs[1].set_xlabel(r"$\hat{c}$", rotation=0)
+axs[1].set_ylabel(r"$c$", rotation=0)
+axs[1].legend()
+figA.savefig(folder + 'Ns_BA_k10.svg', dpi = 900)
+figs.savefig(folder+ 'Ns_BA_collapse_k10.svg', dpi= 900)
 
 # Plot one for ER - various mean k for one N
 # Also do a collapse with multiple displayed on one plot
+
+folder = "Output/ArtificialUniNets/ER/"
+figA, axsA = plt.subplots(1,1)
+
+figs, axs = plt.subplots(1, 2, figsize=(15, 10))
+
+N = 2000
+av_degs = [10,20,40,80]
+
+for i in range(len(av_degs)):
+    g = ER(N, av_degs[i])
+    unagg, agg, agg_pred = package(g)
+    #unagg_HO, agg_HO, agg_pred_HO = package_HO(g)
+
+    plt.figure()
+    plt.plot(unagg[0], unagg[1], 'o', label = r'$\langle k \rangle , N = $' + str(av_degs[i])+', '+str(N)) 
+    plt.xlabel(r"$k$")
+    plt.ylabel(r"$\frac{1}{c}$", rotation=0)
+    plt.xscale("log")
+    plt.legend()
+    plt.savefig(folder + 'ER_Unagg_'+str(N)+'_'+str(av_degs[i])+ '.svg', dpi = 900)
+    plt.close()
+
+    k, c = unagg
+    ks, inv_c_mean, errs = agg
+    ks, inv_c_pred = agg_pred
+    #ks_HO, inv_c_pred_HO = agg_pred_HO
+    axsA.errorbar(ks, inv_c_mean, yerr=errs, fmt='.' ,markersize = 5,capsize=2,color='black')
+    axsA.plot(ks, inv_c_mean,'o', label=r'$\langle k \rangle = $' + str(av_degs[i]))
+    axsA.plot(ks, inv_c_pred,'k--')
+    #axsA.plot(ks_HO, inv_c_pred_HO,'b--')
+
+    y = inv_c_mean/inv_c_pred
+    y_err = errs/inv_c_pred
+    axs[0].errorbar(ks, y, yerr=y_err, fmt='o' ,markersize = 2,capsize=2,color='black')
+    axs[0].plot(ks, y,'o', markersize = 5, label = r'$\langle k \rangle = $' + str(av_degs[i]))
+    
+    c_mean = 1/inv_c_mean
+    c_pred = 1/inv_c_pred
+    axs[1].plot(c_pred, c_mean, 'o', label = r'$\langle k \rangle = $' + str(av_degs[i]))
+
+axsA.legend()
+axsA.set_xlabel(r"$k$")
+axsA.set_ylabel(r"$\frac{1}{c}$", rotation=0)
+axsA.set_xscale("log")
+
+min_k, max_k = axs[0].get_xlim()
+
+fill_x = np.linspace(min_k*0.95, max_k*1.05, 100)
+
+axs[0].fill_between(fill_x, 1-0.05, 1+0.05, color='grey', alpha=0.2, label=r'$\pm 5\%$')
+axs[0].set_xlabel(r"$k$")
+axs[0].set_ylabel(r"$\frac{\hat{c}}{c}$", rotation=0)
+axs[0].legend()
+axs[0].set_xscale("log")
+
+min_y, max_y = axs[1].get_ylim()
+min_x, max_x = axs[1].get_xlim()
+
+c_pred = np.linspace(min_x*0.95, max_x*1.05, 100)
+
+axs[1].plot(c_pred, c_pred, 'k--', label='Expected')
+axs[1].fill_between(c_pred, c_pred*(1-0.05), c_pred*(1+0.05), color='grey', alpha=0.2, label=r'$\pm 5\%$')
+axs[1].set_xlabel(r"$\hat{c}$", rotation=0)
+axs[1].set_ylabel(r"$c$", rotation=0)
+axs[1].legend()
+figA.savefig(folder + 'Av_Deg_ER_N20000.svg', dpi = 900)
+figs.savefig(folder+ 'Av_Deg_ER_collapse_N20000.svg', dpi= 900)
+
+folder = "Output/ArtificialUniNets/ER/"
+figA, axsA = plt.subplots(1,1)
+
+figs, axs = plt.subplots(1, 2, figsize=(15, 10))
+
+Ns = [1000, 2000, 4000, 8000]
+av_deg = 10
+
+for i in range(len(Ns)):
+    g = ER(Ns[i], av_deg)
+    unagg, agg, agg_pred = package(g)
+    #unagg_HO, agg_HO, agg_pred_HO = package_HO(g)
+
+    plt.figure()
+    plt.plot(unagg[0], unagg[1], 'o', label = r'$\langle k \rangle , N = $' + str(Ns[i])+', '+str(av_deg)) 
+    plt.xlabel(r"$k$")
+    plt.ylabel(r"$\frac{1}{c}$", rotation=0)
+    plt.xscale("log")
+    plt.legend()
+    plt.savefig(folder + 'ER_Unagg_'+str(av_deg)+'_'+str(Ns[i])+ '.svg', dpi = 900)
+    plt.close()
+
+    k, c = unagg
+    ks, inv_c_mean, errs = agg
+    ks, inv_c_pred = agg_pred
+    #ks_HO, inv_c_pred_HO = agg_pred_HO
+    axsA.errorbar(ks, inv_c_mean, yerr=errs, fmt='.' ,markersize = 5,capsize=2,color='black')
+    axsA.plot(ks, inv_c_mean,'o', label=r'$N = $' + str(Ns[i]))
+    axsA.plot(ks, inv_c_pred,'k--')
+    #axsA.plot(ks_HO, inv_c_pred_HO,'b--')
+
+    y = inv_c_mean/inv_c_pred
+    y_err = errs/inv_c_pred
+    axs[0].errorbar(ks, y, yerr=y_err, fmt='o' ,markersize = 2,capsize=2,color='black')
+    axs[0].plot(ks, y,'o', markersize = 5, label = r'$N = $' + str(Ns[i]))
+    
+    c_mean = 1/inv_c_mean
+    c_pred = 1/inv_c_pred
+    axs[1].plot(c_pred, c_mean, 'o', label = r'$N = $' + str(Ns[i]))
+
+axsA.legend()
+axsA.set_xlabel(r"$k$")
+axsA.set_ylabel(r"$\frac{1}{c}$", rotation=0)
+axsA.set_xscale("log")
+
+min_k, max_k = axs[0].get_xlim()
+
+fill_x = np.linspace(min_k*0.95, max_k*1.05, 100)
+
+axs[0].fill_between(fill_x, 1-0.05, 1+0.05, color='grey', alpha=0.2, label=r'$\pm 5\%$')
+axs[0].set_xlabel(r"$k$")
+axs[0].set_ylabel(r"$\frac{\hat{c}}{c}$", rotation=0)
+axs[0].legend()
+axs[0].set_xscale("log")
+
+min_y, max_y = axs[1].get_ylim()
+min_x, max_x = axs[1].get_xlim()
+
+c_pred = np.linspace(min_x*0.95, max_x*1.05, 100)
+
+axs[1].plot(c_pred, c_pred, 'k--', label='Expected')
+axs[1].fill_between(c_pred, c_pred*(1-0.05), c_pred*(1+0.05), color='grey', alpha=0.2, label=r'$\pm 5\%$')
+axs[1].set_xlabel(r"$\hat{c}$", rotation=0)
+axs[1].set_ylabel(r"$c$", rotation=0)
+axs[1].legend()
+figA.savefig(folder + 'Ns_ER_k10.svg', dpi = 900)
+figs.savefig(folder+ 'Ns_ER_collapse_k10.svg', dpi= 900)
 
 
 
@@ -94,51 +333,11 @@ plt.close()
 
 
 
+# Plot assortativity vs rchi for BA and ER
+
+
+
 # Contour plot of BA and ER reduced chi squared for various N and mean k
-
-
-
-
-
-
-
-'''
-for i in range(repeats):
-    dfs_BA, rchi_df = run(BA, ns, av_degree, 'BA',to_html=True, to_print=True)
-    dfs_ER, rchi_df2 = run(ER, ns, av_degree, 'ER',to_html=True, to_print=True)
-    #make contour plot of reduced chi squared
-    Zs_BA.append(rchi_df.to_numpy())
-    Zs_ER.append(rchi_df2.to_numpy())
-
-
-Z_BA = np.mean(Zs_BA,axis=0)
-x = np.array(av_degree)
-y = np.array(ns)
-X, Y = np.meshgrid(x, y)
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-surface = ax.plot_surface(X, Y, Z_BA, cmap=cm.coolwarm)
-fig.colorbar(surface, shrink=0.5, aspect=5)
-ax.set_xlabel('Mean k')
-ax.set_ylabel('N')
-ax.set_zlabel('Reduced Chi Squared')
-plt.show()
-
-Z_ER = np.mean(Zs_ER,axis=0)
-x = np.array(av_degree)
-y = np.array(ns)
-X, Y = np.meshgrid(x, y)
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-surface = ax.plot_surface(X, Y, Z_ER, cmap=cm.coolwarm)
-fig.colorbar(surface, shrink=0.5, aspect=5)
-ax.set_xlabel('Mean k')
-ax.set_ylabel('N')
-ax.set_zlabel('Reduced Chi Squared')
-plt.show()
-'''
-
-
 
 
 
